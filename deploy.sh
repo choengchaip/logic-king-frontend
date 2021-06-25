5cfa1a2c35441e3570491a377cdfe247c77a0736
Choengchai Phachonyut <singh@finema.co>
2021-06-26 03:14:42 +0700

    Test build jenkins

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 docker build \
   --build-arg APP_HOST=0.0.0.0 \
   --build-arg APP_PORT=8000 \
-  -t chengchaip/logic-frontend:1.0.3 \
+  -t chengchaip/logic-frontend:1.0.4 \
   -t chengchaip/logic-frontend:latest .
 
-docker push chengchaip/logic-frontend:1.0.3
+docker push chengchaip/logic-frontend:1.0.4
 docker push chengchaip/logic-frontend:latest
